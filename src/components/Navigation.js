92eb65a83aa836b032950e0a4921a36c7ce57339
Elias Meire <me@eliasmei.re>
2017-06-24 15:49:58 +0200

    ik zie logout mimimi

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import { Icon, Menu } from 'antd';
 
-const Navigation = ({ auth, navigate, onClick }) =>
+const Navigation = ({ auth, onClick }) =>
   <Menu
     mode="inline"
     defaultSelectedKeys={['1']}
-    onClick={({ item, key }) => onClick(key)}
+    onClick={({ key }) => onClick(key)}
   >
-    <Menu.Item key="profile">
-      <Icon type="user" />
-      <span className="nav-text">Profile</span>
-    </Menu.Item>
-    <Menu.Item key="app">
-      <Icon type="flag" />
-      <span className="nav-text">Game</span>
-    </Menu.Item>
-    <Menu.Item key="leaderboard">
-      <Icon type="trophy" />
-      <span className="nav-text">Leaderboard</span>
-    </Menu.Item>
-    <Menu.Item key="login">
-      <Icon type="login" />
-      <span className="nav-text">Login</span>
-    </Menu.Item>
-    <Menu.Item key="logout">
-      <Icon type="logout" />
-      <span className="nav-text">Logout</span>
-    </Menu.Item>
+    { auth &&
+      <Menu.Item key="profile">
+        <Icon type="user" />
+        <span className="nav-text">Profile</span>
+      </Menu.Item>
+    }
+    { auth &&
+      <Menu.Item key="app">
+        <Icon type="flag" />
+        <span className="nav-text">Game</span>
+      </Menu.Item>
+    }
+      <Menu.Item key="leaderboard">
+        <Icon type="trophy" />
+        <span className="nav-text">Leaderboard</span>
+      </Menu.Item>
+    { !auth &&
+      <Menu.Item key="login">
+        <Icon type="login" />
+        <span className="nav-text">Login</span>
+      </Menu.Item>
+    }
+    { auth &&
+      <Menu.Item key="logout">
+        <Icon type="logout" />
+        <span className="nav-text">Logout</span>
+      </Menu.Item>
+    }
   </Menu>;
 
 export default Navigation;
