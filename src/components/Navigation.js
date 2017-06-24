0b6e1033e50072b72d38b6c9b302eb8befd7bcba
Elias Meire <me@eliasmei.re>
2017-06-24 17:02:10 +0200

    nice merge

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,15 +7,11 @@ const Navigation = ({ auth, onClick }) =>
     defaultSelectedKeys={['1']}
     onClick={({ key }) => onClick(key)}
   >
-<<<<<<< HEAD
     <Menu.Item key="home">
       <Icon type="home" />
       <span className="nav-text">Home</span>
     </Menu.Item>
     { auth &&
-=======
-    {auth &&
->>>>>>> e8c885162cdd04b321e12faa803c90e7acf6ce07
       <Menu.Item key="profile">
         <Icon type="user" />
         <span className="nav-text">Profile</span>
@@ -23,7 +19,6 @@ const Navigation = ({ auth, onClick }) =>
     {auth &&
       <Menu.Item key="app">
         <Icon type="flag" />
-<<<<<<< HEAD
         <span className="nav-text">Game</span>
       </Menu.Item>
     }
@@ -32,20 +27,6 @@ const Navigation = ({ auth, onClick }) =>
         <span className="nav-text">Leaderboard</span>
       </Menu.Item>
     { auth &&
-=======
-        <span className="nav-text">Run</span>
-      </Menu.Item>}
-    <Menu.Item key="leaderboard">
-      <Icon type="trophy" />
-      <span className="nav-text">Leaderboard</span>
-    </Menu.Item>
-    {!auth &&
-      <Menu.Item key="login">
-        <Icon type="login" />
-        <span className="nav-text">Login</span>
-      </Menu.Item>}
-    {auth &&
->>>>>>> e8c885162cdd04b321e12faa803c90e7acf6ce07
       <Menu.Item key="logout">
         <Icon type="logout" />
         <span className="nav-text">Logout</span>
