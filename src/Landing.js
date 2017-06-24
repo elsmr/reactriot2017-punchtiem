1df818cc230f5d88570fd47bdebfba24121c9765
Elias Meire <me@eliasmei.re>
2017-06-24 23:20:43 +0200

    imagos

--- src/Landing.js
+++ src/Landing.js
@@ -9,7 +9,7 @@ const wrapperStyle = {
 
 const imageStyle = {
   width: '100%',
-  height: 300,
+  height: '30vh',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
@@ -17,52 +17,53 @@ const imageStyle = {
 };
 
 const Landing = ({ auth, history, onLogin }) => {
-  return (
-    <div style={wrapperStyle}>
-      <h1>Monument Run</h1>
+  return <div>
+      <h1 className="structural">Monument Run</h1>
       <Carousel autoplay>
-        <div style={imageStyle}><h3>Explore</h3></div>
-        <div style={imageStyle}><h3>Challenge</h3></div>
-        <div style={imageStyle}><h3>Exercise</h3></div>
+        <div style={{ ...imageStyle, backgroundImage: `url(https://images.unsplash.com/36/xIsiRLngSRWN02yA2BbK_submission-photo-7.jpg?dpr=1&auto=format&fit=crop&w=${window.innerWidth}&q=80&cs=tinysrgb&crop=&bg=)` }}>
+          <h3>Explore</h3>
+        </div>
+        <div style={{ ...imageStyle, backgroundImage: `url(https://images.unsplash.com/photo-1482401204742-eb3c31c24722?dpr=1&auto=compress,format&fit=crop&w=${window.innerWidth}&q=80&cs=tinysrgb&crop=&bg=)` }}>
+          <h3>Challenge</h3>
+        </div>
+        <div style={{ ...imageStyle, backgroundImage: `url(https://images.unsplash.com/photo-1487956382158-bb926046304a?dpr=1&auto=compress,format&fit=crop&w=${window.innerWidth}&q=80&cs=tinysrgb&crop=&bg=)` }}>
+          <h3>Exercise</h3>
+        </div>
       </Carousel>
-      <div
-        style={{ display: 'flex', justifyContent: 'center', margin: '2em 0' }}
-      >
-        <Timeline style={{ textAlign: 'left' }}>
-          <Timeline.Item>Log in using your Google account</Timeline.Item>
-          <Timeline.Item>Start your run</Timeline.Item>
-          <Timeline.Item>
-            Visit as many monuments as possible within the time limit
-          </Timeline.Item>
-          <Timeline.Item>
-            Take pictures of the monuments you've visited
-          </Timeline.Item>
-          <Timeline.Item>Challenge everyone to beat your time!</Timeline.Item>
-        </Timeline>
-      </div>
-      {!auth
-        ? <Button
-            type="primary"
-            onClick={e => {
-              login()
-                .then(result => {
-                  const { credential: { accessToken }, user } = result;
-                  onLogin(accessToken, user);
-                })
-                .catch(error => {
-                  this.setState({
-                    loginError: 'Authentication with Google failed 😢'
+      <div style={wrapperStyle}>
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '2em 0' }}>
+          <Timeline style={{ textAlign: 'left' }}>
+            <Timeline.Item>Log in using your Google account</Timeline.Item>
+            <Timeline.Item>Start your run</Timeline.Item>
+            <Timeline.Item>
+              Visit as many monuments as possible within the time limit
+            </Timeline.Item>
+            <Timeline.Item>
+              Take pictures of the monuments you've visited
+            </Timeline.Item>
+            <Timeline.Item>
+              Challenge everyone to beat your time!
+            </Timeline.Item>
+          </Timeline>
+        </div>
+        {!auth ? <Button type="primary" onClick={e => {
+                login()
+                  .then(result => {
+                    const { credential: { accessToken }, user } = result;
+                    onLogin(accessToken, user);
+                  })
+                  .catch(error => {
+                    this.setState({
+                      loginError: 'Authentication with Google failed 😢'
+                    });
                   });
-                });
-            }}
-          >
-            Login with Google
-          </Button>
-        : <Button type="primary" onClick={() => history.push('/app')}>
-            Start your Monument Run
-          </Button>}
-    </div>
-  );
+              }}>
+              Login with Google
+            </Button> : <Button type="primary" onClick={() => history.push('/app')}>
+              Start your Monument Run
+            </Button>}
+      </div>
+    </div>;
 };
 
 export default Landing;
