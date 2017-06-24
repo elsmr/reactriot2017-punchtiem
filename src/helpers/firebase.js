757e8412d1b59fb2006501b845fa36bb8ccf7add
Elias Meire <me@eliasmei.re>
2017-06-24 10:56:00 +0200

    firebase

---
+++ src/helpers/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyA6vOOiI1nl6gmtFuIPlCUZiZMTC_XUGBo",
+    authDomain: "monumentrun-1cc39.firebaseapp.com",
+    databaseURL: "https://monumentrun-1cc39.firebaseio.com",
+    projectId: "monumentrun-1cc39",
+    storageBucket: "monumentrun-1cc39.appspot.com",
+    messagingSenderId: "430880527135"
+};
+
+firebase.initializeApp(config);
+
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
