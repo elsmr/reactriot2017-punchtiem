37faadb7f79af4c10702bf661e2a33b0e0622146
Haroen Viaene <hello@haroen.me>
2017-06-25 16:51:47 +0200

    servicieer de werker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ConnectedApp from './ConnectedApp';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<ConnectedApp />, document.getElementById('root'));
+registerServiceWorker();
