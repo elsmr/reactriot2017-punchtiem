540faac3872dc0f8ea6894aa5625bd42f2186408
Elias Meire <me@eliasmei.re>
2017-06-25 12:53:37 +0200

    Run detail page 🎉

--- src/components/Run.js
+++ src/components/Run.js
@@ -1,33 +1,80 @@
 import React, { Component } from 'react';
 import { Timeline, Tag } from 'antd';
 import InteractiveMap from './InteractiveMap';
-import Loading from './Loading';
+import LoadingPage from './LoadingPage';
 import { PRIMARY_COLOR } from '../constants';
-import { getScore } from '../helpers/foursquare';
+import { getScore, getVenuePhoto } from '../helpers/foursquare';
+import { ref } from '../helpers/firebase';
+
+const RunNotFound = () => <div style={{ display: 'flex', height: 'calc(100vh - 64px)', justifyContent: 'center', alignItems: 'center' }}><h1>Run not found 😢</h1></div>;
 
 class Run extends Component {
   state = {
     venues: null,
+    venueImages: {},
+    history: null,
     name: null,
-    loading: true
+    loading: true,
+    notFound: false,
+    points: 0
   }
 
   componentWillMount() {
     const { match } = this.props;
+    ref.child(`runs/${match.params.id}`).once('value', this.receiveRun.bind(this), this.runNotFound.bind(this))
+  }
+
+  receiveRun(snapshot) {
+    const record = snapshot.val();
+    if (!record) {
+      this.runNotFound();
+      return;
+    }
+
+    const { history, name, points, venues } = record;
+    this.setState({ venues, history, name, points, loading: false });
+    venues.forEach(venue => {
+      if (!this.state.venueImages.hasOwnProperty(venue.id)) {
+        getVenuePhoto(venue.id).then(res => {
+          if (res.response.photos.count > 0) {
+            const { prefix, suffix } = res.response.photos.items[0];
+            const url = `${prefix}36x36${suffix}`;
+            this.setState(state => ({
+              ...state,
+              venueImages: {
+                ...state.venueImages,
+                [venue.id]: url
+              }
+            }));
+          }
+        });
+      }
+    });
+  }
+
+  runNotFound() {
+    this.setState({ loading: false, notFound: true });
   }
 
   render() {
-    const { loading, name, venues } = this.state;
-    return loading ? <Loading /> : <div>
-          <InteractiveMap />
+    const { notFound, loading, name, venues, venueImages, history, points } = this.state;
+    if (notFound) {
+      return <RunNotFound />;
+    }
+
+    return loading ? <LoadingPage /> : <div>
+          <InteractiveMap venueImages={venueImages} venues={venues} history={history} zoom={16} />
           <div style={{ margin: '2em' }}>
-            <h1>Run by {name}</h1>
+            <h1 style={{ margin: '2em 0', display: 'flex', alignItems: 'center' }}>
+              <span>Run by {name}{' '}</span>
+              <Tag style={{marginLeft: '.5em'}}color={PRIMARY_COLOR}>{points}p</Tag>
+            </h1>
             <Timeline>
-              {venues.map(venue =>
-                <Timeline.Item key={venue.id}>
-                  {venue.name} <Tag color={PRIMARY_COLOR}>{getScore(venue)}</Tag>
-                </Timeline.Item>
-              )}
+              {venues.map(venue => <Timeline.Item key={venue.id}>
+                  {venue.name} <Tag color={PRIMARY_COLOR}>
+                    {getScore(venue.stats)}p
+                  </Tag>
+                </Timeline.Item>)}
             </Timeline>
           </div>
         </div>;
