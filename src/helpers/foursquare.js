e1378c4f76532d95cc2b56270b7df844cf38235e
Haroen Viaene <hello@haroen.me>
2017-06-25 11:11:57 +0200

    heading

--- src/helpers/foursquare.js
+++ src/helpers/foursquare.js
@@ -14,8 +14,27 @@ export const getVenues = ({ latitude, longitude, ...query }) =>
 export const getVenuePhoto = id =>
   foursquare.venues.getVenuePhotos({
     venue_id: id,
-    limit: 1
+    limit: 1,
   });
 
 export const getScore = ({ checkinsCount, usersCount, tipCount }) =>
   checkinsCount + tipCount + usersCount;
+
+const toRadians = degrees => degrees * Math.PI / 180;
+
+export function calculateHeading({ from, to, userHeading }) {
+  const R = 6371e3; // metres
+  const φ1 = toRadians(from.latitude);
+  const φ2 = toRadians(to.latitude);
+  const Δφ = toRadians(to.latitude - from.latitude);
+  const Δλ = toRadians(to.longitude - from.longitude);
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c;
+
+  return d - userHeading;
+}
