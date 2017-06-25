ad288ee82857dddca2a109e46fa07b2847aed306
Haroen Viaene <hello@haroen.me>
2017-06-25 17:48:58 +0200

    no const if no ponst

--- src/helpers/foursquare.js
+++ src/helpers/foursquare.js
@@ -17,15 +17,14 @@ export const getVenuePhoto = id =>
     limit: 1,
   });
 
-export const getScore = ({ checkinsCount, usersCount, tipCount }) => {
+export function getScore({ checkinsCount, usersCount, tipCount }) {
   const weightedCheckins = checkinsCount / 400000.0;
-  const weightedTips = (tipCount / 2000.0) * 3;
+  const weightedTips = tipCount / 2000.0 * 3;
   const weightedUsers = usersCount / 400000.0;
 
   const weightedScore = weightedCheckins + weightedTips + weightedUsers;
   return Math.max(1, Math.ceil(Math.pow(weightedScore * 1000000, 0.2)));
 }
-
 
 const toRadians = degrees => degrees * Math.PI / 180;
 
